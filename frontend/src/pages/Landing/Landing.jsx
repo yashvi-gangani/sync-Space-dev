efba8a8223751696fe0d95592ab7b2d51f41c7e9
kunalkt5656 <kunalkt97@gmail.com>
2026-07-18 11:50:41 +0530

    fix/landingpage

--- frontend/src/pages/Landing/Landing.jsx
+++ frontend/src/pages/Landing/Landing.jsx
@@ -1,35 +1,26 @@
+import { useNavigate } from "react-router-dom";
 import "./Landing.css";
 
 const Landing = () => {
+    const navigate = useNavigate();
 
-    return(
-
+    return (
         <section className="hero">
-
-            <h1>
-                Collaborate Smarter with SyncSpace AI
-            </h1>
+            <h1>Collaborate Smarter with SyncSpace AI</h1>
 
             <p>
-
                 Real-Time Collaborative Whiteboard,
                 Code Editor,
                 Team Chat,
                 Session Replay,
                 and Smart Collaboration in one workspace.
-
             </p>
 
-            <button>
-
-                Get Started
-
+            <button
+                onClick={() => navigate("/login")}>Get Started
             </button>
-
         </section>
-
-    )
-
-}
+    );
+};
 
 export default Landing;
