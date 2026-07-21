abee16f073619c14546e17a2f8f74472e87df1f2
kunalkt5656 <kunalkt97@gmail.com>
2026-07-21 23:50:12 +0530

    bugfix

--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../../services/api";
+import { useAuth } from "../../context/AuthContext";
 import "./Register.css";
 
 const Register = () => {
