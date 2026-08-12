771ae1f4ec9e3ca23da2e8a0d62544549c771b1c
gopichandkuru <gopi76202@gmail.com>
2026-08-12 14:44:47 +0530

    Updated execution route to invoke code execution controller

--- server/src/routes/execution.routes.js
+++ server/src/routes/execution.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const { executeCode } = require('../controllers/execution.controller');
 const router = express.Router();
 
-router.post('/', executeCode);
+router.post('/', executeCode); /// while path is ,create
 
 module.exports = router;
